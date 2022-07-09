633959bb2b1402f11824e12ba27c3fc5bfcfd0d4
Iqbal Hossan <engiqbal1999@gmail.com>
2022-07-09 12:59:56 +0600

    blog crud operation

--- routes/blog.js
+++ routes/blog.js
@@ -21,4 +21,59 @@ router.post("", async (req, res) => {
   }
 });
 
+// GET ALL POST
+router.get("", async (req, res) => {
+  try {
+    const query = {};
+    const blogs = await Blog.find(query);
+    res.status(200).json(blogs);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+// GET SINGLE POST BY ID
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+// DELETE POST BY ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Blog.findByIdAndDelete(id);
+    res.status(200).json("The Post has been deleted!");
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+// UPDATE THE POST
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const updateBlog = await Blog.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { rew: true }
+    );
+    res.status(200).json(updateBlog);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
 module.exports = router;
